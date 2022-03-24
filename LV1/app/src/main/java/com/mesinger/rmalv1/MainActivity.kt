db13705f8c4e25c909188ac668999d4263c46f31
Zvonimir Mesinger <zvonimir.mesinger@gmail.com>
2022-03-24 21:37:48 +0100

    Add Material Design components and finish LV1 excercise

--- LV1/app/src/main/java/com/mesinger/rmalv1/MainActivity.kt
+++ LV1/app/src/main/java/com/mesinger/rmalv1/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.mesinger.rmalv1
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.util.Log
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val lukaImage = findViewById<ImageView>(R.id.lukaImage)
+        val submitButton = findViewById<Button>(R.id.submitButton)
+        val calculateButton = findViewById<Button>(R.id.calculateButton)
+        val description = findViewById<TextView>(R.id.description)
+        val nameSurname = findViewById<TextView>(R.id.nameSurname)
+        val nameSurnameEditText = findViewById<EditText>(R.id.nameSurnameEditText)
+        val descriptionEditText = findViewById<EditText>(R.id.descriptionEditText)
+        val heightEditText = findViewById<EditText>(R.id.heightEditText)
+        val weightEditText = findViewById<EditText>(R.id.weightEditText)
+
+        submitButton.setOnClickListener {
+
+            nameSurname.text = nameSurnameEditText.text
+            description.text = descriptionEditText.text
+
+            Log.d("MainActivity", "submitButton")
+        }
+
+        calculateButton.setOnClickListener {
+            var bmi = calculateBMI(heightEditText.text.toString().toDouble(), weightEditText.text.toString().toDouble()).toString()
+
+            Toast.makeText(this, bmi, Toast.LENGTH_SHORT).show()
+
+            Log.d("MainActivity", "calculateButton")
+        }
+
+        Log.d("MainActivity", "onCreate")
+    }
+
+    private fun calculateBMI(height: Double, weight: Double): Double{
+        var heightInMeters: Double = height/100;
+        Log.d("MainActivity", "calculateBMI")
+        return weight/(heightInMeters*heightInMeters)
+
     }
 }
